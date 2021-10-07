f32f9d4f6e2e5d3e7fd5e57acb29c2844d6de268
Brian <brianjackson1224@gmail.com>
2021-10-07 13:15:00 -0700

    Added fixed date data file

--- cleaning.py
+++ cleaning.py
@@ -6,8 +6,8 @@ valid_marital_status = ["Single", "Together", "Divorced", "Married", "Widow"]
 
 marketing_campaign_data = []
 
-with open('data/marketing_campaign.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='\t', quotechar='|')
+with open('data/marketing_campaign_DATEFIXED.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
         skip = False
         cleanRow = copy.deepcopy(row)
@@ -32,10 +32,11 @@ with open('data/marketing_campaign.csv', newline='') as csvfile:
 
         if not skip:
             marketing_campaign_data.append(cleanRow)
+print(marketing_campaign_data[0])
 
 with open('data/marketing_campaign_CLEANED.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['ID'] + ['Year_Birth'] + ['Education'] + ['Marital_Status'] + ['Income'] + ['Kidhome'] + ['Teenhome'] + ['Dt_Customer'] + ['Recency'] + ['MntWines'] + ['MntFruits'] + ['MntMeatProducts'] + ['MntFishProducts'] + ['MntSweetProducts'] + ['MntGoldProds'] + ['NumDealsPurchases'] + ['NumWebPurchases'] + ['NumCatalogPurchases'] + ['NumStorePurchases'] + ['NumWebVisitsMonth'] + ['AcceptedCmp3'] + ['AcceptedCmp4'] + ['AcceptedCmp5'] + ['AcceptedCmp1'] + ['AcceptedCmp2'] + ['Complain'] + ['Z_CostContact'] + ['Z_Revenue'] + ['Response'])
     for row in marketing_campaign_data:
         # ID	Year_Birth	Education	Marital_Status	Income	Kidhome	Teenhome	Dt_Customer	Recency	MntWines	MntFruits	MntMeatProducts	MntFishProducts	MntSweetProducts	MntGoldProds	NumDealsPurchases	NumWebPurchases	NumCatalogPurchases	NumStorePurchases	NumWebVisitsMonth	AcceptedCmp3	AcceptedCmp4	AcceptedCmp5	AcceptedCmp1	AcceptedCmp2	Complain	Z_CostContact	Z_Revenue	Response
-        writer.writerow([row['ID']] + [row['Year_Birth']] + [row['Education']] + [row['Marital_Status']] + [row['Income']] + [row['Kidhome']] + [row['Teenhome']] + [row['Dt_Customer']] + [row['Recency']] + [row['MntWines']] + [row['MntFruits']] + [row['MntMeatProducts']] + [row['MntFishProducts']] + [row['MntSweetProducts']] + [row['MntGoldProds']] + [row['NumDealsPurchases']] + [row['NumWebPurchases']] + [row['NumCatalogPurchases']] + [row['NumStorePurchases']] + [row['NumWebVisitsMonth']] + [row['AcceptedCmp3']] + [row['AcceptedCmp4']] + [row['AcceptedCmp5']] + [row['AcceptedCmp1']] + [row['AcceptedCmp2']] + [row['Complain']] + [row['Z_CostContact']] + [row['Z_Revenue']] + [row['Response']])
+        writer.writerow([row['\ufeffID']] + [row['Year_Birth']] + [row['Education']] + [row['Marital_Status']] + [row['Income']] + [row['Kidhome']] + [row['Teenhome']] + [row['Dt_Customer']] + [row['Recency']] + [row['MntWines']] + [row['MntFruits']] + [row['MntMeatProducts']] + [row['MntFishProducts']] + [row['MntSweetProducts']] + [row['MntGoldProds']] + [row['NumDealsPurchases']] + [row['NumWebPurchases']] + [row['NumCatalogPurchases']] + [row['NumStorePurchases']] + [row['NumWebVisitsMonth']] + [row['AcceptedCmp3']] + [row['AcceptedCmp4']] + [row['AcceptedCmp5']] + [row['AcceptedCmp1']] + [row['AcceptedCmp2']] + [row['Complain']] + [row['Z_CostContact']] + [row['Z_Revenue']] + [row['Response']])
